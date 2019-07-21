357c9e14d81d3577f8dba3aa794f1bdb0c432603
DrewS <1251956930@qq.com>
2019-07-21 09:48:36 +0800

    重新打包了前端

---
+++ FrontEnd/FE/src/pages/index/components/purchasing.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>这里是购票界面 想要购买的车票ID为{{this.$route.params.id}}</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "purchasing"
+    }
+</script>
+
+<style scoped>
+
+</style>
